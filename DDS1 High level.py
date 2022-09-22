e0fb9465040152fec9335bb3b31a90ff319f1514
Kristiss99 <keltius87@gmail.com>
2022-09-22 13:31:49 +0200

    Create DDS1 High level.py

---
+++ DDS1 High level.py
@@ -0,0 +1,43 @@
+
+def blakly():
+    k = 8
+    R = 0
+    b = 3
+    a = [1,1,0,0,1,1,0,0]
+    n = 55
+
+    for i in range(k):
+        R = 2*R +a[k-1-i] * b 
+        R = R % n 
+    print(R)
+
+def binary():
+    k = 8
+    M = 3
+    n = 55
+    e = [1,1,0,0,1,1,0,0]
+    if e[k-1] == 1:
+        C = M
+    else:
+        C = 1
+    for i in range(k-1):
+        C = C*C % n
+        if e[k-2-i] == 1:
+            C = (C*M) % n
+        print(C)
+    print(C)
+
+
+if __name__ == '__main__':
+    print("Binary:")
+    binary()
+    print("Blakly:")
+    blakly()
+
+
+
+
+
+# Run the program
+
+
